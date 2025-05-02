2ec94040553f481ff43764386f6563797e2077d0
U-BSD\419236 <419236@SHS-8YBF6X3.beaverton.k12.or.us>
2025-05-02 11:47:18 -0700

    work on case 6+

--- redblack.cpp
+++ redblack.cpp
@@ -13,6 +13,7 @@ void caseFour(Node* &cur);
 void caseFive(Node* &cur, Node* &root);
 void insert(Node* &current, int a, Node* &root);
 void remove(Node* M, Node* &root, int x);
+void fixRem(Node* N, Node* &root);
 void print(Node* current, int depth);
 void search(Node* current, int x);
 
@@ -228,7 +229,11 @@ void remove(Node* M, Node* &root, int x) {
 	if (M->getColor() == RED && C->getColor() == BLACK) {
 	  Node* P = M->getParent();
 	  C->setParent(P);
-          if (M != root) { //if M was not root
+	  if (C != M->getRight()) {
+            C->setRight(M->getRight());
+            M->getRight()->setParent(C);
+          }
+	  if (M != root) { //if M was not root
 	    if (P->getRight() == M) {
 	      P->setRight(C);
 	    } else if (P->getLeft() == M) {
@@ -242,6 +247,10 @@ void remove(Node* M, Node* &root, int x) {
 	  Node* P = M->getParent();
 	  C->setColor(BLACK);
 	  C->setParent(P);
+	  if (C != M->getRight()) {
+	    C->setRight(M->getRight());
+	    M->getRight()->setParent(C);
+	  }
 	  if (M != root) { //if M was not root
 	    if (P->getRight() == M) {
 	      P->setRight(C);
@@ -253,12 +262,24 @@ void remove(Node* M, Node* &root, int x) {
 	  }
 	  delete M;
 	} else if (M->getColor() == BLACK && C->getColor() == BLACK) {
-	  if (M == root) { //case1
-	    C->setParent(NULL);
-	    delete M;
-	    root = C;
-          } else if () {
-
+	  if ((M->getRight() == C && M->getLeft() == NULL) ||
+	      (M->getLeft() == C && M->getRight() == NULL)) { //one non-leaf child
+	    if (M == root) { //case1
+	      C->setParent(NULL);
+	      delete M;
+	      root = C;
+	    } else {
+	      //move C into M and rename as N
+	      Node* P = M->getParent();
+	      C->setParent(P);
+	      if (P->getRight() == M) {
+		P->setRight(C);
+	      } else if (P->getLeft() == M) {
+		P->setLeft(C);
+	      }
+	      Node* N = C;
+	      fixRem(N, root);
+	    }
 	  }
 	}
       }
@@ -268,6 +289,68 @@ void remove(Node* M, Node* &root, int x) {
       remove(M->getLeft(), root, x);
     }
   }
+}
+
+void fixRem(Node* N, Node* &root) {
+  Node* P = N->getParent();
+  if (N->getSibling() != NULL) {
+    Node* S = N->getSibling();
+  if (S->getColor() == RED &&
+      P->getColor() == BLACK) { //case2
+    P->setParent(S);
+    P->setColor(RED);
+    S->setColor(BLACK);
+    if (P->getLeft() == N) { //N is on the left
+      P->setRight(S->getLeft());
+      S->getLeft()->setParent(P);
+      S->setLeft(P);
+    } else if (P->getRight() == N) { //N is on the right
+      P->setLeft(S->getRight());
+      S->getRight()->setParent(P);
+      S->setRight(P);
+    }
+  }
+  if (S->getColor() == BLACK &&
+      P->getColor() == BLACK &&
+      (S->getRight() == NULL || S->getRight()->getColor() == BLACK) &&
+      (S->getLeft() == NULL || S->getLeft()->getColor() == BLACK)) { //case3
+    S->setColor(RED);
+    fixRem(P, root);
+  }
+  if (P->getColor() == RED &&
+      (S->getRight() == NULL || S->getRight()->getColor() == BLACK) &&
+      (S->getLeft() == NULL || S->getLeft()->getColor() == BLACK)) { //case4
+    P->setColor(BLACK);
+    S->setColor(RED);
+    return;
+  }
+  //case5
+  if (P->getLeft() == S &&
+      S->getColor() == BLACK &&
+      S->getRight() != NULL && S->getRight()->getColor() == RED &&
+      S->getLeft() != NULL && S->getLeft()->getColor() == BLACK) { //S is on the left
+    Node* SR = S->getRight();
+    Node* SL = S->getLeft();
+    SR->setParent(P);
+    S->setRight(SR->getLeft());
+    SR->setLeft(S);
+    S->setParent(SR);
+    SR->setColor(BLACK);
+    S->setColor(RED);
+  } else if (P->getRight() == S &&
+	     S->getColor() == BLACK &&
+	     S->getLeft() != NULL && S->getLeft()->getColor() == RED &&
+	     S->getRight() != NULL && S->getRight()->getColor() == BLACK) { //S is on the right
+    Node* SR = S->getRight();
+    Node* SL = S->getLeft();
+    SL->setParent(P);
+    S->setLeft(SL->getRight());
+    SL->setRight(S);
+    S->setParent(SL);
+    SL->setColor(BLACK);
+    S->setColor(RED);
+  }
+  //work on case 6/7
 }
 
 void print(Node* current, int depth) {
